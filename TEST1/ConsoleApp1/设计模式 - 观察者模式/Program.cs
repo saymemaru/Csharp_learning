50b0c6227260068b3eb4f06c9aba07cb62122989
saymemaru <innovationthebest@163.com>
2025-09-30 10:57:41 +0800

    添加观察者模式

---
+++ TEST1/ConsoleApp1/设计模式 - 观察者模式/Program.cs
@@ -0,0 +1,13 @@
+﻿using 设计模式___观察者模式;
+
+HealthManager healthManager = new HealthManager();
+
+Player.OnSummoned += healthManager.Player_OnSummoned!;
+Player.OnDestroyed += healthManager.Player_OnDestroyed!;
+Player.OnDamaged += healthManager.Player_OnDamaged!;
+
+Player Tom = new Player("Tom");
+Tom.Damaged(30);
+Tom.Damaged(80);
+
+
---
+++ TEST1/ConsoleApp1/设计模式 - 观察者模式/Sample1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 设计模式___观察者模式
+{
+    class Player : IDisposable
+    {
+        public static event EventHandler <EventArgs> OnSummoned;
+        public static event EventHandler<EventArgs> OnDestroyed;
+        public static event EventHandler <int> OnDamaged;
+        private bool disposed = false;
+        public int Health { get; private set; } = 100;
+        public string Name { get; private set; } = "null";
+
+        public Player(string name)
+        {
+            Name = name;
+            OnSummoned?.Invoke(this,EventArgs.Empty);
+        }
+
+        public void Damaged(int amount) 
+        {
+            Health -= amount;
+            if(Health < 0)
+            {
+                Health = 0;
+                OnDamaged?.Invoke(this, amount);
+                OnDestroyed?.Invoke(this, EventArgs.Empty);
+                Dispose();
+            }
+            else
+            {
+                OnDamaged?.Invoke(this, amount);
+            }
+            
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // 取消事件订阅
+                    // 静态事件的取消需要在外部管理
+                }
+                disposed = true;
+            }
+        }
+    }
+
+    class HealthManager 
+    {
+        List<Player> players = new List<Player>();
+
+        public void Player_OnDestroyed(object sender, EventArgs e)
+        {
+            Player player = (Player)sender;
+            players.Remove(player);
+            Console.WriteLine($"Player {player.Name} has been destroyed, {players.Count} player left now!");
+        }
+        public void Player_OnSummoned(object sender, EventArgs e)
+        {
+            Player player = (Player)sender;
+            players.Add(player);
+            Console.WriteLine($"Player {player.Name} has been summoned,{players.Count} player left now!");
+        }
+        public void Player_OnDamaged(object sender, int e) 
+        {
+            Player player = (Player)sender;
+            Console.WriteLine($"Player {player.Name} took {e} damage and has {player.Health} health left!");
+
+        }
+    }
+
+    
+
+
+
+}
